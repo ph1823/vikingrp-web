4b13553669612a636bf17a937e19d47ac7741b80
Pierre-Henri Dupouy <pierre-henri.dupouy@etu.u-bordeaux.fr>
2023-12-23 11:07:58 +0100

    update depracated import

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
@@ -39,6 +39,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'playerOnline' => 0
         ]);
     }
 }
