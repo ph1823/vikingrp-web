57b553fa4423bc45ad04837ca622ec2244b11e4c
Pierre-Henri Dupouy <pierre-henri.dupouy@etu.u-bordeaux.fr>
2025-05-28 18:57:26 +0200

    feat: add recaptcha

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -20,19 +21,31 @@ class RegistrationController extends BaseController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            $recaptcha_url = "https://www.google.com/recaptcha/api/siteverify";
+            $recaptcha_response = $request->get("g-recaptcha-response");
+            $recaptcha_secret = $_ENV['RECAPTCHA_SECRET_KEY'] ;
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+            // Make and decode POST request:
+            $recaptcha = file_get_contents("$recaptcha_url?secret=$recaptcha_secret&response=$recaptcha_response");
+            $recaptcha = json_decode($recaptcha, null, 512, JSON_THROW_ON_ERROR);
 
-            return $this->redirectToRoute('app_home');
+            if (isset($recaptcha->score) && $recaptcha->score > 0.5) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+
+                return $this->redirectToRoute('app_home');
+            } else {
+                $form->addError(new FormError("Captcha Invalide."));
+            }
         }
 
         return $this->renderBase('registration/register.html.twig', [
